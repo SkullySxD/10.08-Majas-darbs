7d94bd5b801303f39d2d984ce5d2da95a4c6f954
SkullySxD <109369580+SkullySxD@users.noreply.github.com>
2022-08-10 21:50:55 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+import static java.lang.Double.sum;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ievadi skaitli x");
+        int x = scan.nextInt();
+        System.out.println("Ievadi skaitli y");
+        int y = scan.nextInt();
+        System.out.println("Izvēlies matemātisko darbību");
+
+        String mathOp = scan.nextLine();
+
+        switch (mathOp) {
+            case "+":
+                sum(x, y);
+
+
+        }
+
+        int sum = x + y;
+        System.out.println("Rezultāts ir " + sum);
+
+
+    }
+
+
+
+    public static int summ(int x, int y) {
+        return x + y;
+    }
+
+}
